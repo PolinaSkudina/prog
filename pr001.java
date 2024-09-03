371280354d5b1bf30dcf852e5eea3006d9fe40a1
PolinaSkudina <polina.skydina@gmail.com>
2024-09-03 14:11:28 +0700

    Добавлены пременные и примеры деления

--- pr001.java
+++ pr001.java
@@ -1,5 +1,16 @@
 class pr001 {
 	public static void main(String args[]) {
 		System.out.println("Первая программа на Java");
+		//Выводим строку на консоль
+		int Var1 = 1024;
+		int Var2345678;
+		//System.out.println(Var2345678); ОшибкаЖ переменной не присвоено значение
+		Var2345678 = 411;
+		System.out.println(1/2);
+		System.out.println("Результат целоцисленного деления: "+ Var2345678/2);
+		System.out.println(Var2345678/2);
+		double Var3 = (double)Var2345678/2;
+
+
 	}
 }
