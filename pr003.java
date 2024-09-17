3be4242e1f5c55eaf7eb72e809df811883026bba
PolinaSkudina <polina.skydina@gmail.com>
2024-09-17 13:36:20 +0700

    Добавили программу ввода с клавиатуры

---
+++ pr003.java
@@ -0,0 +1,10 @@
+class pr003 {
+	public static void main(String args[]) throws java.io.IOException {
+	
+	//Чтение с клавиатуры
+	char ch;
+	System.out.print("Нажмите клавишу на клавиатуре и нажмите ENTER ");
+	ch = (char) System.in.read();	
+	System.out.print("Вы нажади клавишу: " + ch); 
+	}	
+}		
