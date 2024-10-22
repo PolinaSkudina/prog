9018ad08bc73036a04bdaeab498575b2454504a8
PolinaSkudina <polina.skydina@gmail.com>
2024-10-22 12:59:48 +0700

    Добавили цикл for-earch и поиск значений в массиве

--- pr005.java
+++ pr005.java
@@ -146,5 +146,62 @@ class Arrays {
 			 System.out.println(sample1[i3] +  " ");  
 		 System.out.println();
 		
+
+		// Цикл "for - each"
+		System.out.println();
+		int summ = 0;
+		for (int i3 = 0; i3 < 10; i3 ++)
+			summ += sample3[i3];
+		System.out.println(summ);
+		
+		summ = 0;
+		for (int x: sample3) {
+			System.out.println("Очередное значение:" + x);
+       			summ += x;
+		}
+		System.out.println(summ);
+
+		//for-each для двумерного массива
+		summ = 0;
+		int val = 22;
+		boolean found = false;
+		for(int[] x: riders) {
+			for(int y: x) {
+				if(y == val) {
+					found = true;
+					break;
+				}
+				 System.out.println("Знаение: " + y);
+				 summ += y;
+			}
+			System.out.println("Новая строка массива");
+
+		}
+		System.out.println("Сумма: " + summ);
+		if (found)
+			 System.out.println("Значение найдено");
+
+
+		//Самостоятельный пример поиска значения в одномерном  массивe sample3	
+		System.out.println();
+                summ = 0;
+		val = -2;
+		found = false;
+                for (int x: sample3) {
+			if(x == val) {
+                                found = true;
+                                break;
+			}
+			System.out.println("Очередное значение:" + x);
+			summ += x;
+		}
+		if (found)
+                         System.out.println("Значение найдено");
+
+
+
+
+
 	}
+
 }
