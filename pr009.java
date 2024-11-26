52375c21f8ae64fc8026a80cf852869552d852f5
PolinaSkudina <167408706+PolinaSkudina@users.noreply.github.com>
2024-11-26 14:33:15 +0700

    dobavili primer polimorfisma

--- pr009.java
+++ pr009.java
@@ -1,144 +1,301 @@
-//Основы наследования
 class TwoDShape {
 	private double width;
 	private double height;
-
-	//Конструктор без параметров
-	TwoDShape() {
 	
+	int common;
 
-	//Конструктор суп
+	TwoDShape() {
+		width = height = 0.0;
+	}
+
 	TwoDShape(double w, double h) {
 		width = w;
-		hieght = h;
+		height = h;
+		System.out.println("Внутри конструктора 2DSHAPE(double w, double h) ");
+	}
+
+	TwoDShape(double x) {
+		width = height = x;
+	}
+	
+	TwoDShape(TwoDShape ob) {
+		width = ob.width;
+		height = ob.height;
+	}
+
 	void showDim() {
-		System.out.println("Ширина и высота: " + width + " и " + height);
-		}
+		System.out.println("Ширина и высота: " + width + " " + height);
+	}
 	double getWidth() {
 		return width;
 	}
 	double getHeight() {
 		return height;
 	}
-	void  setWidth(double w) {
-		if(w>200)
+	void setWidth(double w) {
+		if (w > 200)
 			width = 200;
 		else width = w;
 	}
-	void  setHeight(double h) {
-		if(h>290)
+	void setHeight(double h) {
+                if (h > 290)
                         height = 290;
                 else height = h;
+        }
 
-}
 }
 class Triangle extends TwoDShape {
 	String style;
-	//Конструктор 
-	Triangle(String s, double  w, double h) {
-	//Вызов конструктора супер класса
-		suoer (w, h);
-		//Установка значений для перемменой подкласса
-		stile  = s;
-	
-	//Конструктор с одним параметром
-	Triangle (double x) (
+	int common;
+
+	Triangle() {
+		super();
+		style = "отсутствует";
+	}
+
+	Triangle(String s, double w, double h) {
+		super(w, h);
+		style = s;
+		System.out.println("Внутри конструктора Triangle(String s, double w, double h) ");
+	}
+
+	Triangle(double x) {
 		super(x);
-		stile  = "закрашеенный"
+		style = "закрашенный";
+	}
+
+	Triangle(Triangle ob) {
+		super(ob);
+		style = ob.style;
+	}
+
+	Triangle(int a, int b) {
+		super.common = a;
+		common = b;
+		System.out.println("Значение super common: " + super.common);
+		System.out.println("Значение common: " + common);
+	}
+
 	double area() {
-		return getWidth()*getHeight()/2;
+		return getWidth() * getHeight()/2;
 	}
 	void showStyle() {
 		System.out.println("Стиль: " + style);
 	}
 }
 
-class Rectangle extends TwoDShape {
-	String outline;
-	//Конструктор 
-        Rectangle(String o, double  w, double h) {
-		//Вызов конструктора супер класса 
-		super(w, h);
-                //Установка значений переменной подкласса
-                outline  = o;
+//Строгая типизация
+class X {
+	int a;
+	X(int i) {
+		a = i;
+	}
+	void show() {
+		System.out.println("Значение a: " + a);
+	}
+}
+
+class Y {
+	int a;
+	Y(int i) {
+		a = i;
+	}
+}
+
+class Z extends X {
+	int b;
+	Z(int i, int j) {
+		super(j);
+		b = i;
+	}
+	void show() {
+		System.out.println("Значение a и b: " + a + " " + b);
+	}
+}
+
+class ColorTriangle extends Triangle {
+	private String color;
+
+	ColorTriangle (String c, String s, double w, double h) {
+		super(s, w, h);
+		color = c;
+		System.out.println("Внутри конструктора ColorTriangle(String c, String s, double w, double h) ");
 	}
 
-	double area()  {
+	ColorTriangle (ColorTriangle ob) {
+		super(ob);
+		color = ob.color;
+	}
+
+	String getColor() {
+		return color;
+	}
+
+	void showColor() {
+		System.out.println("Цвет: " + color);
+	}
+}
+
+class Rectangle extends TwoDShape {
+	String outline;
+	int common;
+
+	Rectangle() {
+                super();
+                outline = "отсутствует";
+        }
+
+/*        Rectangle(String o, double w, double h) {
+                super(w, h);
+                outline = o;
+        }
+*/
+        Rectangle(double x) {
+                super(x);
+                outline = "сплошная";
+        }
+
+	Rectangle(int a, int b) {
+                super.common = a;
+                common = b;
+
+        }
+
+	Rectangle(String o, double w, double h) {
+		super(w, h);
+		outline = o;
+	}
+
+	double area() {
 		return getWidth()*getHeight();
 	}
 	boolean isSquare() {
-		if (getWidth() == getHeight()) return true;
+		if(getWidth() == getHeight()) return true;
 		return false;
 	}
-	void showOutline() { 
-		System.out.println("Рамка: " + outline);
-	}
+	void showOutline() {
+                System.out.println("Рамка: " + outline);
+        }
 }
 
-class pr009 {
+class pr099 {
 	public static void main(String[] args) {
-		Triangle t1 = new Triangle("закрашенный", 4.0, 4.0);
+		Triangle t1 = new Triangle();
 		Triangle t2 = new Triangle("контурный", 8.0, 12.0);
+		Triangle t3 = new Triangle(4.0);
+		Triangle t4 = new Triangle(5, 10);
+		ColorTriangle t5 = new ColorTriangle("синий", "контурный", 8.0, 12.0);
+		ColorTriangle t6 = new ColorTriangle("красный", "закрашенный", 2.0, 5.0);
+		Triangle t7 = new Triangle(t2);
+		ColorTriangle t8 = new ColorTriangle(t5);
 		Rectangle r1 = new Rectangle("сплошная", 4.0, 4.0);
-		Rectangle r2 = new Rectangle("пунктирная", 8.0, 12.0);
-		 System.out.println();
-		
-	/*	t1.setWidth = 4.0;
-		t1.setHeight = 4.0;
-		t1.style = "закрашенный";
+		Rectangle r2 = new Rectangle(5.0);
 
-		t2.setWidth = 8.0;
-                t2.setHeight = 12.0;
-                t2.style = "контурный";
+		//t1.setWidth(4.0);
+		//t1.setHeight(4.0);
+		//t1.style = "закрашенный";
+	
+		//r1.setWidth(4.0);
+		//r1.setHeight(4.0);
+		//r1.outline = "сплошная";
 
-		r1.setWidth = 4.0;
-                r1.setHeight = 4.0;
-                r1.outline = "сплошная";
+		//r2.setWidth(8.0);
+		//r2.setHeight(12.0);
+		//r2.outline = "пунктирная";
 
+		//t2.setWidth(8.0);
+                //t2.setHeight(12.0);
+                //t2.style = "контурный";
 
-                r2.setWidth = 8.0;
-                r2.setHeight = 12.0;
-                r2.outline = "пунктирная";
-	*/
+		t1 = t2;
 
-		System.out.println("Информация об обекте t1: ");
+		System.out.println("Информация об объекте t1: ");
+
 		t1.showStyle();
 		t1.showDim();
+
 		System.out.println("Площадь: " + t1.area());
 		System.out.println();
-		System.out.println("Информация об обекте t2: ");
-		t2.showStyle();
+
+		System.out.println("Информация об объекте t2: ");
+
+                t2.showStyle();
                 t2.showDim();
-		System.out.println("Площадь: " + t2.area());
+
+                System.out.println("Площадь: " + t2.area());
                 System.out.println();
 
-		System.out.println("Информация об обекте r1: ");
-		r1.showOutline();
-		r1.showDim();
-		if(r1.isSquare())
-			System.out.println("Является квадратом");
-		else
-			System.out.println("Не является квадратом");
-		System.out.println("Площадь: " + r1.area());
-		System.out.println();
-		System.out.println("Информация об обекте r1: ");
+		System.out.println("Информация об объекте t3: ");
 
+                t3.showStyle();
+                t3.showDim();
+
+                System.out.println("Площадь: " + t3.area());
+                System.out.println();
+
+		System.out.println("Информация об объекте t5: ");
+		t5.showStyle();
+		t5.showDim();
+		t5.showColor();
+		System.out.println();
+
+                System.out.println("Информация об объекте t7: ");
+                t7.showStyle();
+                t7.showDim();
+		System.out.println("Площадь: " + t7.area());
+                System.out.println();
+
+		System.out.println("Информация об объекте t8: ");
+                t8.showStyle();
+                t8.showDim();
+		t8.showColor();
+		System.out.println("Площадь: " + t8.area());
+                System.out.println();
+
+
+		System.out.println("Информация об объекте r1: ");
+		r1.showOutline();
+                r1.showDim();
+		if (r1.isSquare())
+			System.out.println("Это квадрат.");
+		else
+			System.out.println("Это не квадрат.");
+
+                System.out.println("Площадь: " + r1.area());
+                System.out.println();
+
+		System.out.println("Информация об объекте r2: ");
 		r2.showOutline();
                 r2.showDim();
-		if(r2.isSquare())
-                        System.out.println("Является квадратом");
+                if (r2.isSquare())
+                        System.out.println("Это квадрат.");
+                else
+                        System.out.println("Это не квадрат.");
 
+                System.out.println("Площадь: " + r2.area());
+                System.out.println();
+		
+		//Строгая типизация
 
+		X x = new X(10);
+		X x2;
+		Y y = new Y(5);
+		Z z = new Z(5,6);
+		x2 = x;
+		x2 = z;
 
+		x.show();
+		System.out.println("При ссылке подкласса");
+		x2.show();
+		x2 = x;
+		System.out.println("При ссылке подкласса");
+		x2.show();
 
+		System.out.println();
 
-
+		System.out.println("x2.a: " + x2.a);
+		//System.out.println("x2.b: " + x2.b);
 	}
 }
 
-
-
-
-
-			
-//
+	
